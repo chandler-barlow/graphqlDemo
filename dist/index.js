3794c8bbec849a535e68addf84d7b25f85798005
chandler-barlow <chandlerbrlw@gmail.com>
2022-10-16 19:21:37 -0500

    forgot to build before deploying

--- dist/index.js
+++ dist/index.js
@@ -7,13 +7,18 @@ const express_1 = __importDefault(require("express"));
 const express_graphql_1 = require("express-graphql");
 const graphql_1 = require("graphql");
 const schema_1 = require("./schema");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = "8081";
+const port = process.env.PORT;
 app.use("/graphql", (0, express_graphql_1.graphqlHTTP)({
     schema: (0, graphql_1.buildSchema)(schema_1.graphQLSchema),
     rootValue: schema_1.graphQLResolvers,
     graphiql: true,
 }));
+app.get("/", (req, res) => {
+    res.send("I'm awake");
+});
 app.use((err, _req, res, _next) => {
     console.error(err.stack);
     res.status(500).send({ error: err });
